57414e3a2298753aea4d90e46d9035e34d556b19
pc <wot.krystof@email.cz>
2020-05-19 07:10:09 +0200

    Update - Incremental

--- Service/Service/Backups-2.0/IncrBackup2.cs
+++ Service/Service/Backups-2.0/IncrBackup2.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Service
 {
-    class IncrBackup2
+    public class IncrBackup2
     {
+        static SnapCompare myCompare = new SnapCompare();
+        public void Copy(string sourceDirectory, string targetDirectory)
+        {
+            var target = new DirectoryInfo(targetDirectory + @"\incr");
+            var source = new DirectoryInfo(sourceDirectory);
+
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                if (!myCompare.IsSnappedFile(fi.FullName, fi.CreationTime.ToString(), Convert.ToString(fi.GetHashCode())))
+                {
+                    var replacementPath = fi.FullName.Replace(targetDirectory, sourceDirectory);
+                    fi.CopyTo(replacementPath);
+                }
+            }
+
+            foreach (DirectoryInfo di in source.GetDirectories())
+            {
+                if (!myCompare.IsSnappedFile(di.FullName, di.CreationTime.ToString(), Convert.ToString(di.GetHashCode())))
+                {
+                    target.CreateSubdirectory(di.Name);
+                }
+                Copy(di.FullName, target.FullName);
+            }
+        }
+        void UpdateSnapshot(DirectoryInfo source)
+        {
+            File.WriteAllText(@"C:\Snap\SnapText.txt", String.Empty);
+            using (StreamWriter sw = new StreamWriter("SnapText.txt"))
+            {
+                foreach (FileInfo fi in source.GetFiles())
+                {
+
+                    File.AppendAllText(@"C:\Snap\SnapText.txt",
+                          fi.FullName + "," + fi.Attributes + "," + fi.CreationTime.ToString() + "," + fi.GetHashCode() + Environment.NewLine);
+
+                }
+                sw.Close();
+
+                foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+                {
+
+                    File.AppendAllText(@"C:\Snap\SnapText.txt",
+                          diSourceSubDir.FullName + "," + diSourceSubDir.Attributes + "," + diSourceSubDir.CreationTime.ToString() + Environment.NewLine);
+                    UpdateSnapshot(diSourceSubDir);
+                }
+                sw.Close();
+            }
+        }
+
     }
 }
