27c9fad6b0af3998cd55da98021b3ce31b6a63f5
Jakobik <62331110+Jakobik@users.noreply.github.com>
2020-05-27 21:27:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+
+namespace Service
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DiffBackup diff = new DiffBackup();
+            FullBackup full = new FullBackup();
+            IncrBackup incr = new IncrBackup();
+            Writer myWriter = new Writer();
+
+
+            //try
+            //{
+            //    Connect.RunCreateAsync().GetAwaiter().GetResult();
+            //    Connect.RunTemplateAsync().GetAwaiter().GetResult();
+
+            //    while (true)
+            //    {
+            //        var sj = new ScheduledJob();
+            //        sj.Run(myWriter.Read("Repetition"));
+
+            //        Console.WriteLine(@"{0}Check Quartz.net\Trace\application.log.txt for Job updates{0}",
+            //                            Environment.NewLine);
+
+            //        Console.WriteLine("{0}Press Ctrl^C to close the window. The job will continue " +
+            //                            "to run via Quartz.Net windows service, " +
+            //                            "see job activity in the Quartz.Net Trace file...{0}",
+            //                            Environment.NewLine);
+
+            //        Thread.Sleep(10000 * 100000);
+            //    }
+            //}
+            //catch (HttpRequestException e)
+            //{
+            //    Console.WriteLine(e.InnerException.Message);
+            //}
+            //Console.ReadLine();
+            //**********************************************************************************************************************************************************************************************
+
+            while (true)
+            {
+
+                ConsoleKeyInfo info = Console.ReadKey();
+
+                if (info.Key == ConsoleKey.NumPad1)
+                    Connect.RunCreateAsync().GetAwaiter().GetResult();
+                try
+                {
+                    if (info.Key == ConsoleKey.NumPad2)
+                    {
+                        if (Convert.ToInt32(File.ReadAllText(@"C:\MaxFull.txt")) - 1 == 0)
+                        {
+                            myWriter.DecreaseInText("MaxFull");
+                            full.Copy(@"C:\1", @"C:\2");
+                            Console.WriteLine("done");
+                        }
+                        else
+                        {
+                            Console.WriteLine("přesažen limit FullBackupu");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                try
+                {
+                    if (info.Key == ConsoleKey.NumPad3)
+                    {
+                        if (Convert.ToInt32(File.ReadAllText(@"C:\MaxSegments.txt")) - 1 == 0)
+                        {
+                            myWriter.DecreaseInText("MaxSegments");
+                            diff.Copy(@"C:\1", @"C:\2");
+                            Console.WriteLine("done");
+                        }
+                        else
+                        {
+                            Console.WriteLine("přesažen limit FullSegments");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                try
+                {
+                    if (info.Key == ConsoleKey.NumPad4)
+                    {
+                        if (Convert.ToInt32(File.ReadAllText(@"C:\MaxSegments.txt")) - 1 == 0)
+                        {
+                            myWriter.DecreaseInText("MaxSegments");
+                            incr.Copy(@"C:\1", @"C:\2");
+                            Console.WriteLine("done");
+                        }
+                        else
+                        {
+                            Console.WriteLine("přesažen limit FullSegments");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                try
+                {
+                    if (info.Key == ConsoleKey.NumPad5)
+                        Connect.RunTemplateAsync().GetAwaiter().GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Report
+    {
+        //static Writer myWriter = new Writer();
+        /*
+         V tom RunRepAsync jsem nevěděl jak budou pojmenovaný ty columns v té databázi tak jsem to hodil jako příklad.
+        */
+
+        static HttpClient client = new HttpClient();
+
+        static async Task<Uri> CreateRepAsync(Klient klient)
+        {
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/klient", klient);
+            response.EnsureSuccessStatusCode();
+
+            return response.Headers.Location;
+        }
+
+        static async Task<Klient> GetKlientAsync(string id)
+        {
+            Klient klient = null;
+            HttpResponseMessage response = await client.GetAsync($"api/Klient/user/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                klient = await response.Content.ReadAsAsync<Klient>();
+            }
+            return klient;
+        }
+
+        static async Task<Klient> PutKlientAsync(Klient klient)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/klient/{klient.id}", klient);
+            response.EnsureSuccessStatusCode();
+
+            klient = await response.Content.ReadAsAsync<Klient>();
+            return klient;
+        }
+
+        static async Task RunRepAsync()
+        {
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Klient klient = new Klient();
+                klient.name = ("nevim co tu dělám");
+                await PutKlientAsync(klient);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
